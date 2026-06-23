37d9d302e71a882dcbebdb838d2c10d4cdd740f4
Drackion <64641342+Drackion@users.noreply.github.com>
2026-06-23 17:14:24 +1000

    Create meshes.js

---
+++ kubejs/server_scripts/mods/bbl_strainers/meshes.js
@@ -0,0 +1,7 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+ServerEvents.tags('item', allthemods => {
+    allthemods.add('strainers:tier_12_meshes', 'kubejs:allthemodium_mesh')
+})
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
