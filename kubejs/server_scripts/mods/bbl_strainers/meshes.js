0dd5b6d0d7b235bd818c05965a4ccccda211b4e9
Drackion <64641342+Drackion@users.noreply.github.com>
2026-06-23 18:23:28 +1000

    Update meshes.js

--- kubejs/server_scripts/mods/bbl_strainers/meshes.js
+++ kubejs/server_scripts/mods/bbl_strainers/meshes.js
@@ -3,5 +3,9 @@
 ServerEvents.tags('item', allthemods => {
     allthemods.add('strainers:tier_12_meshes', 'kubejs:allthemodium_mesh')
 })
+
+ServerEvents.recipes(allthemods => {
+    allthemods.shaped('kubejs:allthemodium_mesh', [' i ', 'imi', ' i '], {i: 'allthemodium:allthemodium_ingot', m: 'strainers:netherite_mesh'})
+})
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
